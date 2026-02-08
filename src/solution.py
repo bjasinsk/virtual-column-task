f0dc962b6bcea099e04667a89f734cc86e236c6b
bjasinsk <01169292@pw.edu.pl>
2026-02-08 21:28:25 +0100

    refactor

--- src/solution.py
+++ src/solution.py
@@ -1,5 +1,5 @@
 import pandas
-from typing import List
+from typing import Optional, List
 import re
 
 OPERATIONS = {
@@ -15,40 +15,37 @@ def check_column_name(name: str) -> bool:
     return True
 
 
-def parse_role(role: str) -> List:
+def parse_role(role: str) -> Optional[List[str]]:
     elements = re.split(r'(\+|\-|\*)', role)
     if len(elements) != 3:
         return None
     
     elements = [e.strip() for e in elements]
-
+    
     first_column, operator, second_column = elements
-
-    for column_name in [first_column, second_column]:
-        if not check_column_name(column_name):
-            return None
+    if not check_column_name(first_column) or not check_column_name(second_column):
+        return None
 
     if operator not in OPERATIONS:
         return None
     
-    return elements
+    return first_column, operator, second_column
 
 
 def add_virtual_column(df: pandas.DataFrame, role: str, new_column: str) -> pandas.DataFrame:
     if not check_column_name(new_column):
         return pandas.DataFrame([])
     
-    new_df = df.copy()
     elements = parse_role(role)
-
     if not elements:
         return pandas.DataFrame([])
-        
+    
     first_column, operator, second_column = elements
 
-    if first_column in df and second_column in df:
-        new_df[new_column] = OPERATIONS[operator](new_df[first_column], new_df[second_column])
-    else:
+    if first_column not in df.columns or second_column not in df.columns:
         return pandas.DataFrame([])
+        
+    new_df = df.copy()
+    new_df[new_column] = OPERATIONS[operator](new_df[first_column], new_df[second_column])
 
     return new_df
